ad9aaa8ae0a0420a7aaa6309fb44009eac370867
Pavan <pavangosangi@gmail.com>
2018-10-10 16:54:16 -0500

    commit

--- shopping-cart/src/app/multipleitems/multipleitems.component.ts
+++ shopping-cart/src/app/multipleitems/multipleitems.component.ts
@@ -7,10 +7,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./multipleitems.component.css']
 })
 export class MultipleitemsComponent implements OnInit {
-
+  product = new Product();
+  products = [this.product, this.product, this.product, this.product, this.product, this.product, this.product,this.product, this.product];
   constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+}
+
+export class Product {
+  itemId = 1;
+  productDescription="These are export quality peanuts.";
+  price = 10.00;
+  productTitle = "Peanuts";
+  productTags = ['peanuts', 'pallilu', 'groundnuts', 'verusenagapappu'];
+  productImgUrl = "https://www.healthline.com/hlcmsresource/images/AN_images/AN288-Peanuts-Shells-732x549-thumb.jpg";
 }
