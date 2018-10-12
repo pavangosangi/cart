da87b11b6497c8e2908cd9ec78c5f8813a0f4cfc
Pavan <pavangosangi@gmail.com>
2018-10-12 16:12:42 -0500

    added image upload code

--- shopping-cart/src/app/addproduct/addproduct.component.ts
+++ shopping-cart/src/app/addproduct/addproduct.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-addproduct',
@@ -8,17 +9,38 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./addproduct.component.css']
 })
 export class AddproductComponent implements OnInit {
-
-  constructor() { }
+  selectedFile: File = null;
+  constructor(private http:HttpClient) { }
 
   form = new FormGroup({
     productTitle: new FormControl(),
     productDescription: new FormControl(),
-    productPrice: new FormControl(),
-    productImgUrl: new FormControl()
+    productPrice: new FormControl()
   })
 
   ngOnInit() {
   }
 
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('',fd,{
+      reportProgress:true,
+      observe: 'events'
+    })
+    .subscribe(event=>{
+      if(event.type === HttpEventType.UploadProgress) {
+        console.log('Upload Progress: '+Math.round(event.loaded / event.total*100)+'%');
+      } else if (event.type === HttpEventType.Response) {
+        console.log(event);
+      }
+    });
+
+  }
+
 }
+ 
